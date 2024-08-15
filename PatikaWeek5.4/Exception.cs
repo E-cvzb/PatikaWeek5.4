85a4a771c44bce9e583c2b8dbbb711f311c40777
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-15 17:26:42 +0300

    Proje dosyası ekle.

---
+++ PatikaWeek5.4/Exception.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek5._4
+{
+    public  class MyException : Exception
+    {
+        
+        public MyException(string text) :base(text) // benim tanımladığım hata mesajı
+        {
+          
+        }
+    }
+}
---
+++ PatikaWeek5.4/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more informationwhile()
+using PatikaWeek5._4;
+
+bool control=false;// döngü için control false tanımladım 
+
+while (!control)// control true olduğunda döngüden çıkar 
+{
+    try
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz.");
+        int number = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(Math.Pow(number,2));//sayının karesi alınma işlemi 
+        control = true;
+    }
+
+    catch (FormatException ex )
+    {
+
+        Console.WriteLine("Hatalı giriş yaptınız lütfen sayı giriniz");// metin ve ya sembol girdiğinde verilecek uyarı
+    }
+    catch(OverflowException ex)
+    {
+        throw new MyException("Lütfen daha büyük ya da daha küçük bir sayı giriniz ");// int 16 ın değer aralığının dışındaki değerler için verilecek uyarı
+    } 
+    // Her koşulda verilecek mesaj
+    finally
+    {
+        Console.WriteLine("İşlemleriniz tamamlandı");
+    }
+}
